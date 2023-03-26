eba369c0b18373f58382f8f6d58e4a09fd982455
DarkNoriss <darknoriss@gmail.com>
2023-03-26 04:02:44 +0200

    fixing the build

--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
 import "./style/main.scss";
 import { createHeader } from "./functions/header";
 import { createFooter } from "./functions/footer";
-import { getWeatherData } from "./functions/weatherData";
 
 window.onload = () => {
   createHeader();
-  // createApp();
   createFooter();
-  // getWeatherData();
 };
