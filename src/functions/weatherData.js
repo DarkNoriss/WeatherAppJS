5148d19a37a75ccacbe0c43ffe45489f72d7a420
DarkNoriss <darknoriss@gmail.com>
2023-03-27 06:36:13 +0200

    added weather conditions from json

--- src/functions/weatherData.js
+++ src/functions/weatherData.js
@@ -1,4 +1,5 @@
 const apiKey = "1986480656ec490d950204923202611";
+let weatherConditions;
 
 export const fetchWeatherData = async (location) => {
   const call = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}&aqi=no`;
@@ -10,3 +11,14 @@ const makeRequest = async (call) => {
     .then((response) => response.json())
     .catch((error) => console.error(error));
 };
+
+export const importWeatherConditions = async () => {
+  await fetch("./src/weather_conditions.json")
+    .then((response) => response.json())
+    .then((data) => {
+      weatherConditions = Object.values(data);
+    })
+    .catch((error) => console.error(error));
+};
+
+export const getWeatherConditions = () => weatherConditions;
